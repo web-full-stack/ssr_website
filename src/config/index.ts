d4ea4da71c49f8865cc72cc80eb3d9d25d64765c
xgqfrms <xgqfrms@outlook.com>
2023-09-15 02:14:30 +0800

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,14 @@
 const Config = {
-  blogCode: "zxm_blog", // 唯一code
-  apiUrl: "http://127.0.0.1:7001", // api服务器地址
+  blogCode: "ssr_blog",
+  apiUrl: "http://127.0.0.1:7001", // API 服务器地址
   spmUrl: "http://127.0.0.1:7003",
 };
 
 if (typeof window !== "undefined") {
   // 浏览器环境下的逻辑 使用外网域名
   if (!window.location.origin.includes(":3000")) {
-    Config.apiUrl = "//x.xxxx.xx";
-    Config.spmUrl = "//x.xxxx.xx";
+    Config.apiUrl = "https://ssr_website.web-full-stack.com/api";
+    Config.spmUrl = "https://ssr_website.web-full-stack.com/spm";
   }
 }
 
